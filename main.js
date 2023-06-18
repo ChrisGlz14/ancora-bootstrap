c7b547a1c8da10aa59fdaeebb930d6377d0489d5
Chrisglz14 <Christiang14@hotmail.com.ar>
2023-06-18 04:32:46 -0300

    animaciones en equipo

---
+++ main.js
@@ -0,0 +1,10 @@
+const cardTomy = document.getElementById("cardTomy");
+const cardPato = document.getElementById("cardPato")
+
+
+cardTomy.classList.add("visible")
+cardTomy.style.transition = "transform 1.5s ease-out";
+cardTomy.style.transform = "translate(30%)";
+
+cardPato.style.transition = "transform 1.5s ease-out";
+cardPato.style.transform = "translate(-30.5%)";
